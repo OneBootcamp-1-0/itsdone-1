819a8b73d12fbd366c40a39b0c36e17ca69e5868
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-09 17:26:51 +0500

    removed App component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-
-const App = () => (
-<>
-  <h1>Hello React</h1>
-</>
-)
-
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById('root'));
