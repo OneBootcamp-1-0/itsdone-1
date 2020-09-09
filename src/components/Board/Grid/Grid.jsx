9a329746a907ace2127a416578fcf18c846e5eaf
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-09 19:54:34 +0500

    fixed spaces in Grid component

--- src/components/Board/Grid/Grid.jsx
+++ src/components/Board/Grid/Grid.jsx
@@ -6,12 +6,12 @@ import Card from '../Card/Card.jsx';
 const Grid = () => {
   return (
     <div className={css.grid}>
-       <Card />
-       <Card />
-       <Card />
-       <Card />
-       <Card />
-       <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
