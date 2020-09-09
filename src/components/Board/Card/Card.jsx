f1d5b7252ffdf8e0c0207ab93a3769c909f1f755
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-09 19:15:18 +0500

    added Card component

---
+++ src/components/Board/Card/Card.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import css from './Card.css';
+
+const Card = () => <div className={css.card}>Не забыть выполнить важную задачу</div>;
+
+export default Card;
