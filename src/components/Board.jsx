b7a79708a26ad7499c2ca37177b516bc9478fda4
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-09 17:27:09 +0500

    added Board component

---
+++ src/components/Board.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../index.css';
+
+const Board = () => {
+  return (
+    <main className='board'>
+      Board
+    </main>
+  );
+};
+
+export default Board;
