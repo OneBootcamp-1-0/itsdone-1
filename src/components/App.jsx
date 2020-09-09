386e96227ca0e427399d23aafb6284843d1534a0
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-09 19:33:47 +0500

    extracted App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../index.css';
-import Header from './Header.jsx';
-import Board from './Board.jsx';
-import Footer from './Footer.jsx';
+import css from './App.css';
+import Header from './Header/Header.jsx';
+import Board from './Board/Board.jsx';
+import Footer from './Footer/Footer.jsx';
+import Grid from './Board/Grid/Grid.jsx';
 
 const App = () => {
   return (
-    <div>
+    <div className={css.page}>
       <Header />
-      <Board />
+      <Board >
+        <Grid type="grid"/>
+      </Board>
       <Footer />
     </div>
   );
