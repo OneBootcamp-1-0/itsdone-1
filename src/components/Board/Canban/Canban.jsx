91a9f810a770ad49d0f4b035109583349a1d4353
mariaYunB <maria.yun.b@gmail.com>
2020-09-10 15:53:10 +0300

    added canban layout

---
+++ src/components/Board/Canban/Canban.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Column from './Column/Column.jsx';
+import css from './Canban.css';
+
+const Canban = () => {
+  return (
+  <div className={css.canban}>
+    <Column title='TODO'/>
+    <Column title='IN PROGRESS'/>
+    <Column title='IN TESTING'/>
+    <Column title='DONE'/>
+    <Column type='newCard'/>
+  </div>
+  );
+};
+
+export default Canban;
