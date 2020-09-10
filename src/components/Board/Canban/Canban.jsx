189ee5ca2f8b3284a305c93f4054dfae8d866bde
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-10 18:30:30 +0500

    fixed spaces in Canban.jsx

--- src/components/Board/Canban/Canban.jsx
+++ src/components/Board/Canban/Canban.jsx
@@ -4,13 +4,13 @@ import css from './Canban.css';
 
 const Canban = () => {
   return (
-  <div className={css.canban}>
-    <Column title='TODO'/>
-    <Column title='IN PROGRESS'/>
-    <Column title='IN TESTING'/>
-    <Column title='DONE'/>
-    <Column type='newCard'/>
-  </div>
+    <div className={css.canban}>
+      <Column title='TODO' />
+      <Column title='IN PROGRESS' />
+      <Column title='IN TESTING' />
+      <Column title='DONE' />
+      <Column type='newCard' />
+    </div>
   );
 };
 
