b8ff781528f01b295e54b6fda88635a97520bb60
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-11 22:04:32 +0500

    added grid component to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header/Header.jsx';
 import Board from './components/Board/Board.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import Canban from './components/Board/Canban/Canban.jsx';
+import Grid from './components/Board/Grid/Grid.jsx';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Header />
       <Board>
         <Canban />
+        <Grid />
       </Board>
       <Footer />
     </div>
