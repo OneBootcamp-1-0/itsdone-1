310712aa83d46c4df2f556e673c4b4694991547f
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-11 22:12:07 +0500

    added conditions to Card component

--- src/components/Board/Card/Card.jsx
+++ src/components/Board/Card/Card.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import css from './Card.css';
+  
+const Card = props => {
+  const {isButton} = props;
 
-const Card = () => {
   return (
-    <div className={css.card}>
+    <div className={`${css.card} ${isButton ? null : css.card_canban}`}>
       <p className={css.card__date}>Завтра в 14:00</p>
       <h2 className={css.card__title}>Не забыть выполнить важную задачу</h2>
       <p className={css.card__note}>Перед выполнением нужно внимательно прочитать документацию.</p>
-      <button className={css.card__btn} type='button'>Done!</button>
+      {isButton ? <button className={css.card__btn} type='button'>Done!</button> : null}
     </div>
-  )
-}
+  );
+};
 
 export default Card;
