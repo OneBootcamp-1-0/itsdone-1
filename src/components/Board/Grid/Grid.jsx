f0be93534512696046cfff2ffc4df6d084a4649e
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-12 21:43:33 +0500

    added Cards rendering in Grid

--- src/components/Board/Grid/Grid.jsx
+++ src/components/Board/Grid/Grid.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import css from './Grid.css';
 import GridCard from '../Card/GridCard.jsx';
 import NewCard from '../NewCard/NewCard.jsx';
+import createData from '../../../data.js';
 
 const Grid = () => {
+  const data = createData();
+
   return (
     <div className={css.grid}>
       <NewCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
+      {data.cards.map((card, i) => {
+        return <GridCard key={i} date={card.date} title={card.title} text={card.text} />
+      })}
     </div>
   );
 };
