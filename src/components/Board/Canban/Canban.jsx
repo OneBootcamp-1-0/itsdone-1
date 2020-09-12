a52556dc57aed9fe95a13ed661ad2f7fd2802f81
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-12 21:41:13 +0500

    added Columns and Cards rendering

--- src/components/Board/Canban/Canban.jsx
+++ src/components/Board/Canban/Canban.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Column from './Column/Column.jsx';
 import css from './Canban.css';
+import createData from '../../../data.js';
+import Card from '../Card/CanbanCard.jsx';
 
 const Canban = () => {
+  const data = createData();
+  const createFilteredCards = (columnStatus) => {
+    return data.cards.map((card, i) => {
+      if (columnStatus === card.status) {
+        return <Card key={i} date={card.date} title={card.title} text={card.text} />
+      }
+    });
+  };
+
   return (
     <div className={css.canban}>
-      <Column title='TODO' />
-      <Column title='IN PROGRESS' />
-      <Column title='IN TESTING' />
-      <Column title='DONE' />
-      <Column type='newCard' />
+      {data.columns.map((column, i) => {
+        return <Column key={i} title={column.title}>
+          {createFilteredCards(column.status)}
+        </ Column>
+      })}
     </div>
   );
 };
