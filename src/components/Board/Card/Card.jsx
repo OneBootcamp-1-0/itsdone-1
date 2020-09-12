883427d656ddea5b82086eb2b026a49e2406c940
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-12 21:42:46 +0500

    changed data inserting in card

--- src/components/Board/Card/Card.jsx
+++ src/components/Board/Card/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import css from './Card.css';
-  
+
 const Card = props => {
-  const {isButton} = props;
+  const { isButton, date, title, text, isDone } = props;
 
   return (
     <div className={`${css.card} ${isButton ? null : css.card_canban}`}>
-      <p className={css.card__date}>Завтра в 14:00</p>
-      <h2 className={css.card__title}>Не забыть выполнить важную задачу</h2>
-      <p className={css.card__note}>Перед выполнением нужно внимательно прочитать документацию.</p>
-      {isButton ? <button className={css.card__btn} type='button'>Done!</button> : null}
+      <p className={css.card__date}>{date}</p>
+      <h2 className={css.card__title}>{title}</h2>
+      <p className={css.card__note}>{text}</p>
+      {isButton ? <button className={css.card__btn} type='button'>{isDone ? 'NOTDONE!' : 'DONE!'}</button> : null}
     </div>
   );
 };
