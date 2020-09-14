c89eee681b57f405e1cd6b08fc0980c5c9dcfa67
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-14 13:54:54 +0500

    added classes for done and not done cards

--- src/components/Board/Card/Card.jsx
+++ src/components/Board/Card/Card.jsx
@@ -5,10 +5,13 @@ const Card = props => {
   const { isButton, date, title, text, isDone } = props;
 
   return (
-    <div className={`${css.card} ${isButton ? null : css.card_canban}`}>
-      <p className={css.card__date}>{date}</p>
-      <h2 className={css.card__title}>{title}</h2>
-      <p className={css.card__note}>{text}</p>
+    <div className={`${css.card} ${isButton ? '' : css.card_canban}`}>
+      <p className={`${css.card__date} ${isDone ? css.card__done : ''}`}>{date}</p>
+      <h2 className={`${css.card__title} ${isDone ? css.card__done : ''}`}>{title}</h2>
+      <div className={css.card__note_wrapper}>
+        <div className={css.card__note_shadow}></div>
+        <p className={`${css.card__note} ${isDone ? css.card__done : ''}`}>{text}</p>
+      </div>
       {isButton ? <button className={css.card__btn} type='button'>{isDone ? 'NOTDONE!' : 'DONE!'}</button> : null}
     </div>
   );
