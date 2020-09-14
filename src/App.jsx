5f5a293a5de70eed78c673c59065fb7cd7a756ac
Daria <dasha_kravchenko_00@mail.ru>
2020-09-14 18:16:02 +0300

    added onCardEdit prop to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,20 @@ import Footer from './components/Footer/Footer.jsx';
 import Canban from './components/Board/Canban/Canban.jsx';
 import Grid from './components/Board/Grid/Grid.jsx';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import { getColumns, getCards } from './data.js';
+import { getColumns } from './data.js';
 
-const App = () => {
-  const cards = getCards();
+const App = props => {
+  const { onCardEdit, cards } = props;
+  const [localCards, setLocalCards] = useState(cards);
+  setLocalCards(cards);
 
   return (
     <div className={css.page}>
       <Header />
       <Board>
         <Switch>
-          <Route exact path="/canban" render={() => <Canban columns={getColumns()} cards={cards}/>} />
-          <Route exact path="/grid" render={() => <Grid cards={cards} />} />
+          <Route exact path="/canban" render={() => <Canban onCardEdit={onCardEdit} columns={getColumns()} cards={cards}/>} />
+          <Route exact path="/grid" render={() => <Grid onCardEdit={onCardEdit} cards={cards} />} />
           <Route path="/" render={() => <Redirect to="/grid" />} />
         </Switch>
       </Board>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import { Router } from 'react-router-dom';
 import {createBrowserHistory} from 'history';
+import { getCards } from './data.js';
 
 const history = createBrowserHistory();
 
+const state = {
+  cards: getCards(),
+};
+
+const onCardEdit = (cardId, cardIsDone) => {
+  const foundCard = state.cards.find(card => {
+    return card.id === cardId;
+  });
+
+  let cards = state.cards;
+  cards = [...cards, {...foundCard, isDone: cardIsDone}];
+};
+
 ReactDOM.render(
-  <Router history={history}> 
-      <App />
+  <Router history={history}>
+      <App reRender={reRender} cards={state.cards} onCardEdit={onCardEdit} />
   </Router>
   , document.getElementById('root'));
