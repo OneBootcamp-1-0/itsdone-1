7bed4a683f1b8ce54317f1836757c4be70276db8
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-14 13:21:38 +0500

    refactored Grid.jsx

--- src/components/Board/Grid/Grid.jsx
+++ src/components/Board/Grid/Grid.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import css from './Grid.css';
 import GridCard from '../Card/GridCard.jsx';
 import NewCard from '../NewCard/NewCard.jsx';
-import createData from '../../../data.js';
+import { getCards } from '../../../data.js';
 
 const Grid = () => {
-  const data = createData();
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    setCards(getCards());
+  }, []);
 
   return (
     <div className={css.grid}>
       <NewCard />
-      {data.cards.map((card, i) => {
-        return <GridCard key={i} date={card.date} title={card.title} text={card.text} />
+      {cards.map((card, i) => {
+        return <GridCard key={i} isDone={card.isDone} date={card.date} title={card.title} text={card.text} />
       })}
     </div>
   );
