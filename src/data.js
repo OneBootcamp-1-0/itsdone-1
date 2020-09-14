f8d198e34bb22acb308126073c5b880a2804a162
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-14 13:20:04 +0500

    refactored data.js

--- src/data.js
+++ src/data.js
@@ -1,46 +1,51 @@
-const getRandomElement = (arr, min, max) => {
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+const statusesList = ['toDo', 'inProgress', 'inTesting', 'done'];
+const statusesToColumnTitles = {
+  toDo: 'TODO',
+  inProgress: 'IN PROGRESS',
+  inTesting: 'IN TESTING',
+  done: 'DONE'
+};
+const textsList = [
+  'Перед выполнением нужно внимательно прочитать документацию.',
+  'авно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст..',
+  'Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.', 'Есть много надо'
+];
+const titlesList = [
+  'Не забыть выполнить важную задачу',
+  'Постирать кошку',
+  'Сделать задание',
+  'Найти покемона'
+];
+const datesList = [
+  'Завтра в 14:00',
+  'Cегодня в 18:45',
+  'Вчера в 3:00',
+  ''
+];
+
+const getRandomElementFromArray = (arr) => {
+  const randomNumber = Math.floor(Math.random() * (arr.length - 0) + 0);
+
   return arr[randomNumber];
 };
 
-const createData = (count = 20) => {
-  const data = {
-    cards: [],
-    columns: []
-  };
-  const statusList = ['toDo', 'inProgress', 'inTesting', 'done'];
-  const textList = [
-    'Перед выполнением нужно внимательно прочитать документацию.',
-    'авно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст..',
-    'Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.', 'Есть много надо'
-  ];
-
-  const statusToTitle = {
-    toDo: 'TODO',
-    inProgress: 'IN PROGRESS',
-    inTesting: 'IN TESTING',
-    done: 'DONE'
-  };
-
-statusList.forEach(status => {
-  const column = {
-    title: statusToTitle[status],
-    status: status
-  }
-  data.columns.push(column);
- });
-
-  for (let i = 0; i < count; i++) {
-    const card = {
-      date: 'Завтра в 14:00',
-      title: 'Не забыть выполнить важную задачу',
-      text: getRandomElement(textList, 0, textList.length - 1),
-      status: getRandomElement(statusList, 0, statusList.length - 1),
-      isDone: false
-    }
-    data.cards.push(card);
-  }
-  return data;
+export const getCards = (quantity = 20) => {
+  return Array.from({ length: quantity }).map(() => {
+    return {
+      date: getRandomElementFromArray(datesList),
+      title: getRandomElementFromArray(titlesList),
+      text: getRandomElementFromArray(textsList),
+      status: getRandomElementFromArray(statusesList),
+      isDone: getRandomElementFromArray([true, false])
+    };
+  });
 };
 
-export default createData;
+export const getColumns = () => {
+  return statusesList.map(status => {
+    return {
+      title: statusesToColumnTitles[status],
+      status
+    };
+  });
+};
