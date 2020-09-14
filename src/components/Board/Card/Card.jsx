c42fdb633ac2ac514f43f34673c7f71fe3aeabe6
Daria <dasha_kravchenko_00@mail.ru>
2020-09-14 18:17:59 +0300

    added btnClick handler to Card.jsx

--- src/components/Board/Card/Card.jsx
+++ src/components/Board/Card/Card.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import css from './Card.css';
 
 const Card = props => {
-  const { isButton, date, title, text, isDone } = props;
+  const { isButton, date, title, text, isDone, id, onCardEdit } = props;
+
+  const onBtnClick = e => {
+    const card = e.target.closest('.card');
+
+    onCardEdit(Number(card.dataset.id), card.dataset.done === 'true' ? 'qweqew' : 'weqweqefsdfasf');
+  };
 
   return (
-    <div className={`${css.card} ${isButton ? '' : css.card_canban}`}>
+    <div data-id={id} data-done={isDone} className={`${css.card} ${isButton ? '' : css.card_canban} card`}>
       <p className={`${css.card__date} ${isDone ? css.card__done : ''}`}>{date}</p>
       <h2 className={`${css.card__title} ${isDone ? css.card__done : ''}`}>{title}</h2>
       <div className={css.card__note_wrapper}>
         <div className={css.card__note_shadow}></div>
         <p className={`${css.card__note} ${isDone ? css.card__done : ''}`}>{text}</p>
       </div>
-      {isButton ? <button className={css.card__btn} type='button'>{isDone ? 'NOTDONE!' : 'DONE!'}</button> : null}
+      {isButton ? <button className={css.card__btn} type='button' onClick={onBtnClick}>{isDone ? 'NOTDONE!' : 'DONE!'}</button> : null}
     </div>
   );
 };
