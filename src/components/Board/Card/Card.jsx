4be6fd110226f13bd0225251a16ef91535c198e1
mariaYunB <maria.yun.b@gmail.com>
2020-09-15 14:03:23 +0300

    data is taken from props

--- src/components/Board/Card/Card.jsx
+++ src/components/Board/Card/Card.jsx
@@ -4,14 +4,12 @@ import css from './Card.css';
 const Card = props => {
   const { isButton, date, title, text, isDone, id, onCardEdit } = props;
 
-  const onBtnClick = e => {
-    const card = e.target.closest('.card');
-
-    onCardEdit(Number(card.dataset.id), card.dataset.done === 'true' ? false : true);
+  const onBtnClick = () => {
+    onCardEdit(id, !isDone);
   };
 
   return (
-    <div data-id={id} data-done={isDone} className={`${css.card} ${isButton ? '' : css.card_canban} card`}>
+    <div className={`${css.card} ${isButton ? '' : css.card_canban} card`}>
       <p className={`${css.card__date} ${isDone ? css.card__done : ''}`}>{date}</p>
       <h2 className={`${css.card__title} ${isDone ? css.card__done : ''}`}>{title}</h2>
       <div className={css.card__note_wrapper}>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './Header.css';
 import logo from '../../img/logo.svg';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-  const gridLinkRef = React.createRef();
-  const canbanLinkRef = React.createRef();
+  const [ activeLink, setActiveLink ] = useState('grid');
 
   const onGridLinkClick = () => {
-    const canbanDataSet = canbanLinkRef.current.dataset;
-    if (canbanDataSet.activeLink === "true") {
-      canbanDataSet.activeLink = "false";
-      gridLinkRef.current.dataset.activeLink = "true";
+    if (activeLink === 'canban') {
+      setActiveLink('grid');
     }
   };
 
   const onCanbanLinkClick = () => {
-    const gridDataSet = gridLinkRef.current.dataset;
-    if (gridDataSet.activeLink === "true") {
-      gridDataSet.activeLink = "false";
-      canbanLinkRef.current.dataset.activeLink = "true";
+    if (activeLink === 'grid') {
+      setActiveLink('canban');
     }
   };
 
@@ -29,8 +24,8 @@ const Header = () => {
         <img src={logo} width='172' height='48' alt='It`s done!'></img>
       </div>
       <div>
-        <NavLink data-active-link="true" ref={gridLinkRef} onClick={onGridLinkClick} className={`${css.nav_link} ${css.nav_link_active}`} to='/grid'>Grid</NavLink>
-        <NavLink data-active-link="false" ref={canbanLinkRef} onClick={onCanbanLinkClick} className={css.nav_link} to='/canban'>Canban</NavLink>
+        <NavLink data-active-link={activeLink === 'grid' ? true : false} onClick={onGridLinkClick} className={`${css.nav_link} ${css.nav_link_active}`} to='/grid'>Grid</NavLink>
+        <NavLink data-active-link={activeLink === 'canban' ? true : false} onClick={onCanbanLinkClick} className={css.nav_link} to='/canban'>Canban</NavLink>
       </div>
     </header>
   );
