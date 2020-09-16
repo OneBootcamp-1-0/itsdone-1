f1a66ca00d256c4d8f3ea9020f8d9f2e46a4ab33
Daria <dasha_kravchenko_00@mail.ru>
2020-09-16 19:04:46 +0300

    added edit  mode for card

--- src/components/Board/Card/Card.jsx
+++ src/components/Board/Card/Card.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import css from './Card.css';
+import EditCard from './EditCard.jsx';
+
 
 const Card = props => {
 
@@ -8,17 +10,31 @@ const Card = props => {
   const { isButton, date, title, text, isDone, id, onCardEdit } = props;
 
   const onBtnClick = () => {
-    onCardEdit(id, !isDone);
+    onCardEdit(id, {isDone: !isDone});
   };
+
+  const onCardClick = e => {
+    if (e.target.closest('div[data-card=true]') && !e.target.closest('button')) {
+      setIsEdit(true);
+    }
+  }
+
+  const closeEditCard = e => {
+    if (!e.target.closest('div[data-card=true]')) {
+      setIsEdit(false);
+    }
+  };
+
+  document.querySelector('#root').addEventListener('click', (e) => {
+    closeEditCard(e);
+  });
 
   const onDragStart = (e) => {
     e.persist();
     e.dataTransfer.setData('card_id', e.target.id);
   }
 
-
-  return  (
-    <div id={id} draggable={true} onDragStart={onDragStart} className={`${css.card} ${isButton ? '' : css.card_canban} card`}>
+return isEdit ? <EditCard setIsEdit={setIsEdit} id={id} onCardEdit={onCardEdit} date={date} title={title} text={text}/> : <div data-card={true} id={id} onClick={onCardClick} draggable={true} onDragStart={onDragStart} className={`${css.card} ${isButton ? '' : css.card_canban} card`}>
       <p className={`${css.card__date} ${isDone ? css.card__done : ''}`}>{date}</p>
       <h2 className={`${css.card__title} ${isDone ? css.card__done : ''}`}>{title}</h2>
       <div className={css.card__note_wrapper}>
@@ -27,7 +43,6 @@ const Card = props => {
       </div>
       {isButton ? <button className={css.card__btn} type='button' onClick={onBtnClick}>{isDone ? 'NOTDONE!' : 'DONE!'}</button> : null}
     </div>
-  );
 };
 
 export default Card;
