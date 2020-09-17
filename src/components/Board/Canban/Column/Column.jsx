b09a6e5b6cf8a9f87c8b95f784f6438777bf92a7
Daria <dasha_kravchenko_00@mail.ru>
2020-09-17 17:00:27 +0300

    removed EditCard to Grid n Column

--- src/components/Board/Canban/Column/Column.jsx
+++ src/components/Board/Canban/Column/Column.jsx
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import css from './Column.css';
 import CanbanCard from '../../Card/CanbanCard.jsx';
+import EditCard from '../../Card/EditCard.jsx';
+
 
 const Column = props => {
   const { status, title, onCardEdit } = props;
+
+  const [isEdit, setIsEdit] = useState({
+    id: null,
+    isEdit: false
+  });
+
+  useEffect(() => {
+    document.querySelector('#root').addEventListener('click', (e) => {
+      if (!isEdit.id && !e.target.closest('div[data-card=true]') && !e.target.closest('input') && !e.target.closest('textarea') && !e.target.closest('button')) {
+        setIsEdit({
+          id: isEdit.id,
+          isEdit: false
+        })
+      }
+    });
+  },[])
 
   const columnRef = React.createRef();
 
@@ -25,8 +43,11 @@ const Column = props => {
       <h1 className={css.column__title}>{title}</h1>
       <div ref={columnRef} className={css.column__cards} droppable="true" onDragOver={onDragOver} onDrop={onDrop}>
         {props.cards.map((card, i) => {
+          if (card.id === isEdit.id && isEdit.isEdit) {
+            return <EditCard key={i} setIsEdit={setIsEdit} id={card.id} onCardEdit={onCardEdit} date={card.date} title={card.title} text={card.text}/>
+          }
           if (card.status === status) {
-            return <CanbanCard onCardEdit={onCardEdit} key={i} id={card.id} date={card.date} title={card.title} text={card.text} />
+            return <CanbanCard setIsEdit={setIsEdit} onCardEdit={onCardEdit} key={i} id={card.id} date={card.date} title={card.title} text={card.text} />
           }
         })}
       </div>
--- src/components/Board/Grid/Grid.jsx
+++ src/components/Board/Grid/Grid.jsx
@@ -2,15 +2,35 @@ import React, { useState, useEffect } from 'react';
 import css from './Grid.css';
 import GridCard from '../Card/GridCard.jsx';
 import NewCard from '../NewCard/NewCard.jsx';
+import EditCard from '../Card/EditCard.jsx';
 
 const Grid = props => {
   const { cards, onCardEdit } = props;
+
+  const [isEdit, setIsEdit] = useState({
+    id: null,
+    isEdit: false
+  });
+
+  useEffect(() => {
+    document.querySelector('#root').addEventListener('click', (e) => {
+      if (!isEdit.id && !e.target.closest('div[data-card=true]') && !e.target.closest('input') && !e.target.closest('textarea') && !e.target.closest('button')) {
+        setIsEdit({
+          id: isEdit.id,
+          isEdit: false
+        })
+      }
+    });
+  },[]);
 
   return (
     <div className={css.grid}>
       <NewCard />
       {cards.map((card, i) => {
-        return <GridCard onCardEdit={onCardEdit} id={card.id} key={i} isDone={card.isDone} date={card.date} title={card.title} text={card.text} />
+        if (card.id === isEdit.id && isEdit.isEdit) {
+          return <EditCard key={i} setIsEdit={setIsEdit} id={card.id} onCardEdit={onCardEdit} date={card.date} title={card.title} text={card.text}/>
+        }
+        return <GridCard setIsEdit={setIsEdit} onCardEdit={onCardEdit} id={card.id} key={i} isDone={card.isDone} date={card.date} title={card.title} text={card.text} />
       })}
     </div>
   );
