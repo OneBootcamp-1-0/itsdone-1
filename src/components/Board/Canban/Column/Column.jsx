6a1076897254efad459f665f61934fcb148892f0
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-18 14:11:36 +0500

    removed querySelector from Column

--- src/components/Board/Canban/Column/Column.jsx
+++ src/components/Board/Canban/Column/Column.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import css from './Column.css';
 import CanbanCard from '../../Card/CanbanCard.jsx';
 import EditCard from '../../Card/EditCard.jsx';
@@ -7,23 +7,10 @@ import EditCard from '../../Card/EditCard.jsx';
 const Column = props => {
   const { status, title, onCardEdit } = props;
 
-  const [isEdit, setIsEdit] = useState({
+  const [editCard, setEditCard] = useState({
     id: null,
     isEdit: false
   });
-
-  useEffect(() => {
-    document.querySelector('#root').addEventListener('click', (e) => {
-      if (!isEdit.id && !e.target.closest('div[data-card=true]') && !e.target.closest('input') && !e.target.closest('textarea') && !e.target.closest('button')) {
-        setIsEdit({
-          id: isEdit.id,
-          isEdit: false
-        })
-      }
-    });
-  },[])
-
-  const columnRef = React.createRef();
 
   const onDrop = e => {
     e.preventDefault();
@@ -41,13 +28,13 @@ const Column = props => {
   return (
     <div>
       <h1 className={css.column__title}>{title}</h1>
-      <div ref={columnRef} className={css.column__cards} droppable="true" onDragOver={onDragOver} onDrop={onDrop}>
+      <div className={css.column__cards} droppable="true" onDragOver={onDragOver} onDrop={onDrop}>
         {props.cards.map((card, i) => {
-          if (card.id === isEdit.id && isEdit.isEdit) {
-            return <EditCard key={i} setIsEdit={setIsEdit} id={card.id} onCardEdit={onCardEdit} date={card.date} title={card.title} text={card.text}/>
+          if (card.id === editCard.id && editCard.isEdit) {
+            return <EditCard key={i} setEditCard={setEditCard} id={card.id} onCardEdit={onCardEdit} date={card.date} title={card.title} text={card.text}/>
           }
           if (card.status === status) {
-            return <CanbanCard setIsEdit={setIsEdit} onCardEdit={onCardEdit} key={i} id={card.id} date={card.date} title={card.title} text={card.text} />
+            return <CanbanCard setEditCard={setEditCard} onCardEdit={onCardEdit} key={i} id={card.id} date={card.date} title={card.title} text={card.text} />
           }
         })}
       </div>
