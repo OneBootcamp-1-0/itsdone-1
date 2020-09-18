ab8a23ecc0b44ee2e938e252fbb24a1df3a2e019
mariaYunB <maria.yun.b@gmail.com>
2020-09-18 11:19:09 +0300

    added EditCard component to NewCard

--- src/components/Board/NewCard/NewCard.jsx
+++ src/components/Board/NewCard/NewCard.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './NewCard.css';
+import EditCard from './../Card/EditCard.jsx'
 
-const NewCard = () => {
-  return (
-    <button className={css.new_card}>+ ADDNEW</button>
-  );
+const NewCard = props => {
+  const {id, onCardEdit } = props;
+
+  const [isEdit, setIsEdit] = useState(false);
+
+  const onNewCardClick = e => {
+    if (e.target.closest('button')) {
+      setIsEdit(true);
+    }
+  };
+
+  return isEdit
+    ? <EditCard setIsEdit={setIsEdit} id={id} onCardEdit={onCardEdit} date='' title='' text=''/>
+    : <button className={css.new_card} onClick={onNewCardClick}>+ ADDNEW</button>
+
 };
 
 export default NewCard;
