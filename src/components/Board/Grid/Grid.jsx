cc48ba3ea9640416e1c93d6524113dc158487a3a
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-18 14:13:30 +0500

    removed querySelector and isEdit changed to editCard

--- src/components/Board/Grid/Grid.jsx
+++ src/components/Board/Grid/Grid.jsx
@@ -7,30 +7,19 @@ import EditCard from '../Card/EditCard.jsx';
 const Grid = props => {
   const { cards, onCardEdit } = props;
 
-  const [isEdit, setIsEdit] = useState({
+  const [editCard, setEditCard] = useState({
     id: null,
     isEdit: false
   });
-
-  useEffect(() => {
-    document.querySelector('#root').addEventListener('click', (e) => {
-      if (!isEdit.id && !e.target.closest('div[data-card=true]') && !e.target.closest('input') && !e.target.closest('textarea') && !e.target.closest('button')) {
-        setIsEdit({
-          id: isEdit.id,
-          isEdit: false
-        })
-      }
-    });
-  },[]);
 
   return (
     <div className={css.grid}>
       <NewCard />
       {cards.map((card, i) => {
-        if (card.id === isEdit.id && isEdit.isEdit) {
-          return <EditCard key={i} setIsEdit={setIsEdit} id={card.id} onCardEdit={onCardEdit} date={card.date} title={card.title} text={card.text}/>
+        if (card.id === editCard.id && editCard.isEdit) {
+          return <EditCard key={i} setEditCard={setEditCard} id={card.id} onCardEdit={onCardEdit} date={card.date} title={card.title} text={card.text}/>
         }
-        return <GridCard setIsEdit={setIsEdit} onCardEdit={onCardEdit} id={card.id} key={i} isDone={card.isDone} date={card.date} title={card.title} text={card.text} />
+        return <GridCard setEditCard={setEditCard} onCardEdit={onCardEdit} id={card.id} key={i} isDone={card.isDone} date={card.date} title={card.title} text={card.text} />
       })}
     </div>
   );
