3810b68ca542731da0881aa941961ec2e0371c26
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-21 19:24:33 +0500

    added bodyParser middleware

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const tasks = require('./api/tasks');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const PORT = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(404).json({
