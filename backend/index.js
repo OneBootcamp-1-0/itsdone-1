7d8f17b9aef25896bd00dcebe8797c6050c446ca
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-21 21:47:15 +0500

    added cors middleware

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const tasks = require('./api/tasks');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 
 const PORT = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(404).json({
