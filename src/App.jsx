d0b94353bae21c6e6720fafec2cbbc95c4d5c5e4
Daria <dasha_kravchenko_00@mail.ru>
2020-09-21 17:54:59 +0300

    created function for show completed button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import css from './App.css';
 import Header from './components/Header/Header.jsx';
@@ -10,14 +10,15 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 const App = props => {
   const { onCardEdit, cards, columns } = props;
+  const [hideCompleted, setHideCompleted] = useState(false);
 
   return (
     <div className={css.page}>
-      <Header />
+      <Header setHideCompleted={setHideCompleted}/>
       <Board>
         <Switch>
           <Route exact path="/canban" render={() => <Canban onCardEdit={onCardEdit} columns={columns} cards={cards}/>} />
-          <Route exact path="/grid" render={() => <Grid onCardEdit={onCardEdit} cards={cards} />} />
+          <Route exact path="/grid" render={() => <Grid hideCompleted={hideCompleted} onCardEdit={onCardEdit} cards={cards} />} />
           <Route path="/" render={() => <Redirect to="/grid" />} />
         </Switch>
       </Board>
