0203b52029577129a52b07e563df6f7eecc81e4a
Daria <dasha_kravchenko_00@mail.ru>
2020-09-21 17:54:11 +0300

    added condition for card rendering

--- src/components/Board/Grid/Grid.jsx
+++ src/components/Board/Grid/Grid.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import css from './Grid.css';
 import GridCard from '../Card/GridCard.jsx';
 import NewCard from '../NewCard/NewCard.jsx';
 import EditCard from '../Card/EditCard.jsx';
 
 const Grid = props => {
-  const { cards, onCardEdit } = props;
+  const { cards, onCardEdit, hideCompleted } = props;
 
   const [editCard, setEditCard] = useState({
     id: null,
@@ -19,7 +19,11 @@ const Grid = props => {
         if (card.id === editCard.id && editCard.isEdit) {
           return <EditCard key={i} setEditCard={setEditCard} id={card.id} onCardEdit={onCardEdit} date={card.date} title={card.title} text={card.text} />
         }
-        return <GridCard setEditCard={setEditCard} onCardEdit={onCardEdit} id={card.id} key={i} isDone={card.isDone} date={card.date} title={card.title} text={card.text} />
+        if (hideCompleted) {
+          return <GridCard setEditCard={setEditCard} onCardEdit={onCardEdit} id={card.id} key={i} isDone={card.isDone} date={card.date} title={card.title} text={card.text} />
+        } else if (!card.isDone) {
+            return  <GridCard setEditCard={setEditCard} onCardEdit={onCardEdit} id={card.id} key={i} isDone={card.isDone} date={card.date} title={card.title} text={card.text} />
+          }
       })}
     </div>
   );
