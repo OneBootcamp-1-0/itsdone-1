0b94d03411933d93cb2a9c8f1cbbea4e14aad30c
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-21 17:25:12 +0500

    added tasks route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const tasks = require('./api/tasks');
 
 const app = express();
 
 const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.status(404).json({
+    message: 'Not found' 
+  });
+});
+
+app.use('/tasks', tasks);
 
 app.listen(PORT);
