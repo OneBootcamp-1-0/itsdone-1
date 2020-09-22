cf57841c6117229b698bb77b2fe63309cef12fe6
mariaYunB <maria.yun.b@gmail.com>
2020-09-22 21:26:45 +0300

    added Block component

---
+++ src/components/Board/Schedule/Block/Block.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import css from './Block.css';
+import GridCard from '../../Card/GridCard.jsx';
+import EditCard from '../../Card/EditCard.jsx';
+
+const Block = props => {
+  const { title, onCardEdit } = props;
+
+  const [editCard, setEditCard] = useState({
+    id: null,
+    isEdit: false
+  });
+
+  return (
+    <div>
+      <h1 className={css.block__title}>{title}</h1>
+      <div className={css.block}>
+      {props.cards.map((card, i) => {
+        if (card.id === editCard.id && editCard.isEdit) {
+          return <EditCard key={i} setEditCard={setEditCard} id={card.id} onCardEdit={onCardEdit} date={card.date} title={card.title} text={card.text} />
+        }
+        return <GridCard setEditCard={setEditCard} onCardEdit={onCardEdit} id={card.id} key={i} isDone={card.isDone} date={card.date} title={card.title} text={card.text} />
+      })}
+      </div>
+    </div>
+  );
+};
+
+export default Block;
