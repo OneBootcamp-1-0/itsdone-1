c9cdd9a99a16e3df4f62db42bb8cd4578d777e89
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-22 21:33:19 +0500

    added separated api layer

---
+++ src/api/tasks-api.js
@@ -0,0 +1,19 @@
+export const tasksAPI = {
+  getTasks: () => {
+    return fetch('http://localhost:3000/tasks').then(res => res.json());
+  },
+  addTask: task => {
+    return fetch('http://localhost:3000/tasks', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task)
+    }).then(res => res.json());
+  },
+  updateTask: task => {
+    return fetch(`http://localhost:3000/tasks/${task.id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task)
+    }).then(res => res.json());
+  }
+};
