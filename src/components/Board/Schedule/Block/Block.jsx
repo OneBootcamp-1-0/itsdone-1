9024e7f1cfe45950409105a803b5bdeeae58171b
mariaYunB <maria.yun.b@gmail.com>
2020-09-22 21:47:04 +0300

    NewCard rendering moved from Schedule to Block

--- src/components/Board/Schedule/Block/Block.jsx
+++ src/components/Board/Schedule/Block/Block.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import css from './Block.css';
 import GridCard from '../../Card/GridCard.jsx';
 import EditCard from '../../Card/EditCard.jsx';
+import NewCard from '../../NewCard/NewCard.jsx';
 
 const Block = props => {
   const { title, onCardEdit } = props;
@@ -15,6 +16,7 @@ const Block = props => {
     <div>
       <h1 className={css.block__title}>{title}</h1>
       <div className={css.block}>
+      {title === "NO DATE" ? <NewCard /> : null}
       {props.cards.map((card, i) => {
         if (card.id === editCard.id && editCard.isEdit) {
           return <EditCard key={i} setEditCard={setEditCard} id={card.id} onCardEdit={onCardEdit} date={card.date} title={card.title} text={card.text} />
--- src/components/Board/Schedule/Schedule.jsx
+++ src/components/Board/Schedule/Schedule.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import Block from './Block/Block.jsx';
 import css from './Schedule.css';
-import NewCard from '../NewCard/NewCard.jsx';
 
 const Schedule = props => {
   const { cards, blocks, onCardEdit } = props;
 
   return (
     <div className={css.schedule}>
-      <div>
-        <NewCard />
-      </div>
       {blocks.map((block, i) => <Block onCardEdit={onCardEdit} cards={cards} key={i} title={block.title} />)}
     </div>
   );
