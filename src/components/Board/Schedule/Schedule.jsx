edd34e38ce2de208603e376afe69727f6d73ec6a
Daria <dasha_kravchenko_00@mail.ru>
2020-09-23 16:31:15 +0300

    added filters for blocks

--- src/components/Board/Schedule/Schedule.jsx
+++ src/components/Board/Schedule/Schedule.jsx
@@ -4,6 +4,19 @@ import css from './Schedule.css';
 
 const Schedule = props => {
   const { cards, blocks, onCardEdit } = props;
+  let date = new Date();
+
+  const filterLaterThisWeek = () => {
+    return cards.filter(card => Date.parse(card.date) > date && Date.parse(card.date) < date.setDate(date.getDate() + 7));
+  };
+
+  const filterLaterThisMonth = () => {
+    return cards.filter(card => Date.parse(card.date) > date && Date.parse(card.date) < date.setMonth(date.getMonth() + 1));
+  }
+
+  const filterUpcomingMonths = () => {
+    return cards.filter(card => Date.parse(card.date) > date.setMonth(date.getMonth() + 1));
+  }
 
   return (
     <div className={css.schedule}>
