3c543cf7b47b738dcd3c7dcd6e25fd1f2114c8dd
Daria <dasha_kravchenko_00@mail.ru>
2020-09-25 17:28:03 +0300

    added allTags to props

--- src/components/Board/Schedule/Schedule.jsx
+++ src/components/Board/Schedule/Schedule.jsx
@@ -3,11 +3,11 @@ import Block from './Block/Block.jsx';
 import css from './Schedule.css';
 
 const Schedule = props => {
-  const { cards, blocks, onCardEdit } = props;
+  const { cards, blocks, onCardEdit, allTags } = props;
 
   return (
     <div className={css.schedule}>
-      {blocks.map((block, i) => <Block onCardEdit={onCardEdit} cards={cards} key={i} title={block.title} />)}
+      {blocks.map((block, i) => <Block allTags={allTags} onCardEdit={onCardEdit} cards={cards} key={i} title={block.title} />)}
     </div>
   );
 };
