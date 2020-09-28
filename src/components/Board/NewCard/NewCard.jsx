4fe4db03d8f07c9bb1c9725c7bb816de5ddbcea9
mariaYunB <maria.yun.b@gmail.com>
2020-09-28 16:54:28 +0300

    setIsEdit func replaced with setEditCard function

--- src/components/Board/NewCard/NewCard.jsx
+++ src/components/Board/NewCard/NewCard.jsx
@@ -5,16 +5,16 @@ import EditCard from './../Card/EditCard.jsx'
 const NewCard = props => {
   const { id } = props;
 
-  const [isEdit, setIsEdit] = useState(false);
+  const [isEdit, setEditCard] = useState(false);
 
   const onNewCardClick = e => {
     if (e.target.closest('button')) {
-      setIsEdit(true);
+      setEditCard(true);
     }
   };
 
   return isEdit
-    ? <EditCard isNewCard={true} setIsEdit={setIsEdit} id={id} date='' title='' text='' tags={[]} />
+    ? <EditCard isNewCard={true} setEditCard={setEditCard} id={id} date='' title='' text='' tags={[]} />
     : <button className={css.new_card} onClick={onNewCardClick}>+ ADDNEW</button>
 
 };
