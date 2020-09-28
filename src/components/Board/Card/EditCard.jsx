33fc6cf9b5ce068e1ae1ca93c5e5bae5156557bd
mariaYunB <maria.yun.b@gmail.com>
2020-09-28 15:37:15 +0300

    EditCard buttons translated into Russian

--- src/components/Board/Card/EditCard.jsx
+++ src/components/Board/Card/EditCard.jsx
@@ -63,8 +63,8 @@ const EditCard = props => {
         <input type='text' className={css.card__title} onChange={e => onInputChange(e.target.value, 'title')} value={formVal.title} />
         <textarea cols='20' rows='4' className={css.card__edit_note} onChange={e => onInputChange(e.target.value, 'text')} value={formVal.text} />
         <input type="text" onChange={e => onInputChange(e.target.value, 'tags')} value={formVal.tags} />
-        <button type='submit' className={css.card__btn}>Сохранить</button>
-        <button data-cancelbtn={true} onClick={closeEditCard} type='submit' className={css.card__btn}>Отменить</button>
+        <button type='submit' className={css.card__btn}>Save</button>
+        <button data-cancelbtn={true} onClick={closeEditCard} type='submit' className={`${css.card__btn} ${css.card__btn_cancel}`}>Cancel</button>
       </form>
     </div>
   )
