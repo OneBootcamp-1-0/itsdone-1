7abb8881e49d96213112032f1dccb46f44e9ca3f
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-09-29 15:27:47 +0500

    changed mapping in TagCloud

--- src/components/Board/Card/TagCloud.jsx
+++ src/components/Board/Card/TagCloud.jsx
@@ -3,21 +3,15 @@ import css from './TagCloud.css';
 
 const TagCloud = props => {
 
-  const {tags} = props;
+  const { tags } = props;
 
-  const getColor = () => {
-    const r = Math.floor(Math.random() * (256));
-    const g = Math.floor(Math.random() * (256));
-    const b = Math.floor(Math.random() * (256));
-    const color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
-    return color;
-  };
-
-  const tagItems = tags.map((tag,i) => <li className={css.tag_cloud__item} style={{color : getColor()}} key={i}>{tag}</li>)
+  const tagItems = Object.keys(tags).map((tagName, i) => {
+    return <li className={css.tag_cloud__item} style={{ color: tags[tagName] }} key={i}>{tagName}</li>
+  });
 
   return (
     <ul className={css.tag_cloud}>
-      {tagItems}
+      {tagItems.length ? tagItems : null}
     </ul>
   )
 };
