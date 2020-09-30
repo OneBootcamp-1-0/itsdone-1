6c0c42db2381d7d5ea612bef4f96cb3a1a05eb24
mariaYunB <maria.yun.b@gmail.com>
2020-09-30 19:55:01 +0300

    added title validation

--- src/components/Board/Card/EditCard.jsx
+++ src/components/Board/Card/EditCard.jsx
@@ -16,16 +16,19 @@ const EditCard = props => {
   const onFormSubmit = e => {
     e.preventDefault();
     e.persist();
-    if (isNewCard) {
-      dispatch(operations.addTask({ ...formVal, tags: formVal.tags.split(' ') }));
-    } else {
-      dispatch(operations.updateTask({ id: id, ...formVal, tags: formVal.tags.split(' ') }));
+    if (formVal.title.trim().length > 0) {
+      if (isNewCard) {
+        dispatch(operations.addTask({ ...formVal, tags: formVal.tags.split(' ') }));
+      } else {
+        dispatch(operations.updateTask({ id: id, ...formVal, tags: formVal.tags.split(' ') }));
+      }
+      setEditCard({
+        id: id,
+        isEdit: false
+      });
     }
-    setEditCard({
-      id: id,
-      isEdit: false
-    });
   }
+
 
   const onInputChange = (value, type) => {
     if (type === 'tags') {
@@ -41,6 +44,14 @@ const EditCard = props => {
         if (validArr.indexOf(false) === -1) {
           setFormVal({ ...formVal, [type]: value });
         }
+      }
+    } else if (type === 'title') {
+      if (value.trim().length < 1) {
+        document.querySelector('input[data-title=true]').setCustomValidity('must be filled')
+        setFormVal({ ...formVal, [type]: value });
+      } else {
+        document.querySelector('input[data-title=true]').setCustomValidity('');
+        setFormVal({ ...formVal, [type]: value });
       }
     } else {
       setFormVal({ ...formVal, [type]: value });
@@ -60,7 +71,7 @@ const EditCard = props => {
     <div className={css.card}>
       <form onSubmit={onFormSubmit} className={css.card__form}>
         <input type='date' className={css.card__date} onChange={e => onInputChange(e.target.value, 'date')} value={formVal.date} />
-        <input type='text' className={css.card__title} onChange={e => onInputChange(e.target.value, 'title')} value={formVal.title} required />
+        <input data-title={true} type='text' className={css.card__title} onChange={e => onInputChange(e.target.value, 'title')} value={formVal.title} required />
         <textarea cols='20' rows='4' className={css.card__edit_note} onChange={e => onInputChange(e.target.value, 'text')} value={formVal.text} />
         <input type="text" onChange={e => onInputChange(e.target.value, 'tags')} value={formVal.tags} />
         <div className={css.card__btn_group}>
