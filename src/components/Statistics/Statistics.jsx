b16629b6210b0befa36952571b82284a1867184f
Daria <dasha_kravchenko_00@mail.ru>
2020-10-01 13:38:02 +0300

    added statistics list

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -20,6 +20,18 @@ const Statistics = () => {
     <div className={css.statistics}>
       <div ref={statBlock} className={css.statistics__inner}>
         <button className={css.statistics__btn} onClick={hideStat}>HIDE STATISTICS</button>
+        <ul className={css.statistics__list}>
+          <li>Tasks created <span>400</span></li>
+          <li>Tasks completed
+            <ul className={css.completed__list}>
+              <li>Last week <span>20</span></li>
+              <li>Last month <span>68</span></li>
+              <li>Last year <span>310</span></li>
+              <li>Total <span>310</span></li>
+            </ul>
+          </li>
+          <li>Completed tasks ratio <span>77%</span></li>
+        </ul>
       </div>
       <button ref={showStatBtn} className={`${css.statistics__btn} ${css.statistics__btn__show}`} onClick={showStat}>SHOW STATISTICS</button>
     </div>
