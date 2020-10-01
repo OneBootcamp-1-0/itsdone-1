b7a404397e7b9d11558507b4135626942987cc6f
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-10-01 13:01:59 +0500

    validation moved to onChange event

--- src/components/Board/Card/EditCard.jsx
+++ src/components/Board/Card/EditCard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { operations } from '../../../redux/tasksReducer';
 import css from './Card.css';
@@ -23,19 +23,6 @@ const EditCard = props => {
   };
 
   const tagsInputRef = useRef();
-  // Write your own popups. customValidity sucks
-  const onInvalidTagsInput = () => {
-    if (tagsInputRef.current.validity.patternMismatch) {
-      const tagsNames = formVal.tags.trim().split(' ');
-      const isValid = tagsNames.every(tagName => tagName.length < 10);
-
-      if (tagsNames.length > 5) {
-        tagsInputRef.current.setCustomValidity('You have too many tags. Max quantity is 5');
-      } else {
-        tagsInputRef.current.setCustomValidity('Some of your tags are too long. Max length of each tag is 9 characters');
-      }
-    }
-  };
 
   const onFormSubmit = e => {
     e.preventDefault();
@@ -73,7 +60,22 @@ const EditCard = props => {
   }
 
   const onInputChange = (value, type) => {
-    setFormVal({ ...formVal, [type]: value });
+    if (type === 'tags') {
+      const tagsNames = value.trim().split(' ');
+      const isValid = tagsNames.every(tagName => tagName.split('').splice(1).join('').length < 10);
+
+      if (tagsNames.length > 5) {
+        tagsInputRef.current.setCustomValidity('You have too many tags. Max quantity is 5');
+      } else if (!isValid) {
+        tagsInputRef.current.setCustomValidity('Some of your tags are too long. Max length of each tag is 9 characters');
+      } else {
+        tagsInputRef.current.setCustomValidity('');
+      }
+
+      setFormVal({ ...formVal, [type]: value });
+    } else {
+      setFormVal({ ...formVal, [type]: value });
+    }
   }
 
   const closeEditCard = e => {
@@ -91,7 +93,7 @@ const EditCard = props => {
         <input type='date' className={css.card__date} onChange={e => onInputChange(e.target.value, 'date')} value={formVal.date} />
         <input type='text' className={css.card__title} onChange={e => onInputChange(e.target.value, 'title')} value={formVal.title} required />
         <textarea cols='20' rows='4' className={css.card__edit_note} onChange={e => onInputChange(e.target.value, 'text')} value={formVal.text} />
-        <input ref={tagsInputRef} onInvalid={onInvalidTagsInput} pattern="(\S{1,9}\s?){0,5}" type="text" onChange={e => onInputChange(e.target.value, 'tags')} value={formVal.tags} />
+        <input ref={tagsInputRef} type="text" onChange={e => onInputChange(e.target.value, 'tags')} value={formVal.tags} />
         <button type='submit' className={css.card__btn}>Save</button>
         <button data-cancelbtn={true} onClick={closeEditCard} type='submit' className={`${css.card__btn} ${css.card__btn_cancel}`}>Cancel</button>
       </form>
