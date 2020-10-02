1b74d0c6b71e101a195ab78c12c0b6123ce420d0
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-10-02 18:24:16 +0500

    fixed spaces in EditCard

--- src/components/Board/Card/EditCard.jsx
+++ src/components/Board/Card/EditCard.jsx
@@ -18,17 +18,17 @@ const EditCard = props => {
   const getRandomColor = ranges => {
     if (!ranges) {
       ranges = [
-          [50,256],
-          [30, 150],
-          [180, 240]
+        [50, 256],
+        [30, 150],
+        [180, 240]
       ];
     }
     const g = () => {
-        const range = ranges.splice(Math.floor(Math.random()*ranges.length), 1)[0];
-        return Math.floor(Math.random() * (range[1] - range[0])) + range[0];
+      const range = ranges.splice(Math.floor(Math.random() * ranges.length), 1)[0];
+      return Math.floor(Math.random() * (range[1] - range[0])) + range[0];
     };
     return '#' + g().toString(16) + g().toString(16) + g().toString(16);
-};
+  };
 
   const tagsInputRef = React.createRef();
 
