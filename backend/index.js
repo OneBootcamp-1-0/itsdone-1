06f7718590ae47a870cf1afb65f5f736217833f5
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-10-09 00:22:33 +0500

    added webSockets on server

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,32 @@
 const express = require('express');
-const tasks = require('./api/tasks');
+const http = require('http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
 const path = require('path');
+const webSocket = require('ws');
+const data = require('./data.json');
+
+const app = express();
+const server = http.createServer(app);
+
+const wss = new webSocket.Server({ server });
+
+wss.on('connection', ws => {
+  console.log('Client connected');
+
+  ws.on('error', err => {
+    console.error(err)
+  });
+
+  ws.on('message', () => {
+    wss.clients.forEach(client => client.send(JSON.stringify(data.tasks)));
+  });
+});
 
 let port = process.env.PORT;
 
 if (!port) {
-  port = 8000;
+  port = 8080;
 }
 
 app.use(bodyParser.json());
@@ -21,18 +39,93 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use('/tasks', tasks);
+app.get('/tasks', (req, res) => {
+  try {
+    res.status(200).json(data.tasks);
+  } catch (err) {
+    res.status(500).json({
+      message: 'Error'
+    });
+  }
+});
+
+app.post('/tasks', (req, res) => {
+  try {
+    const newTask = req.body;
+    const tasks = data.tasks;
+
+    const newModifiedTask = { ...newTask, id: tasks.length, date: newTask.date ? new Date(newTask.date).toISOString() : '' };
+
+    tasks.push(newModifiedTask);
+
+    res.status(201).json({
+      message: 'New task successfully created',
+      newTask: newModifiedTask
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: 'Error'
+    });
+  }
+});
+
+app.patch('/tasks/:id', (req, res) => {
+  try {
+    let newTask = req.body;
+
+    data.tasks = data.tasks.map(task => {
+      if (task.id === Number(req.params.id)) {
+        const isDonePropEmpty = newTask.isDone === undefined;
+        if (!newTask.status && !isDonePropEmpty && newTask.isDone !== task.isDone) {
+          newTask.status = newTask.isDone ? 'done' : 'toDo';
+        }
+        newTask = { ...task, ...newTask };
+        newTask.date = newTask.date ? new Date(newTask.date).toISOString() : '';
+
+        return newTask;
+      }
+      return task;
+    });
+
+    res.status(200).json({
+      message: 'Task successfully updated',
+      updatedTask: newTask
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: 'Error'
+    });
+  }
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  try {
+    const taskId = Number(req.params.id);
+
+    data.tasks.splice(taskId, 1);
+    data.tasks = data.tasks.map((task, i) => ({ ...task, id: i }));
+
+    res.status(200).json({
+      message: 'Task successfully deleted',
+      deletedTaskId: taskId
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: 'Error'
+    });
+  }
+});
 
 app.get('/grid', (req, res) => {
-  res.sendFile('index.html', {root: path.join(__dirname, '/../build')})
+  res.sendFile('index.html', { root: path.join(__dirname, '/../build') })
 });
 
 app.get('/canban', (req, res) => {
-  res.sendFile('index.html', {root: path.join(__dirname, '/../build')})
+  res.sendFile('index.html', { root: path.join(__dirname, '/../build') })
 });
 
 app.get('/schedule', (req, res) => {
-  res.sendFile('index.html', {root: path.join(__dirname, '/../build')})
+  res.sendFile('index.html', { root: path.join(__dirname, '/../build') })
 });
 
-app.listen(port);
+server.listen(port);
