5197156a1c35337544b6ac21a5c2b08c99a9625b
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-11-03 23:00:58 +0500

    createWebSocketConnection removed from index

--- src/index.js
+++ src/index.js
@@ -4,31 +4,18 @@ import App from './App.jsx';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
+import { operations } from './redux/tasksReducer.js';
 import store from './redux/store.js';
 
 const history = createBrowserHistory();
-let ws = new WebSocket('wss://murmuring-brushlands-70389.herokuapp.com/');
 
-const createWebSocketConnection = (onMessage, onClose) => {
-  ws = new WebSocket('wss://murmuring-brushlands-70389.herokuapp.com/');
-  ws.onmessage = onMessage;
-  ws.onclose = () => {
-    ws = null;
-    onClose(onMessage);
-    renderDOM();
-  }
-};
-
-const renderDOM = () => {
+store.dispatch(operations.initWS());
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-        <App ws={ws} createWebSocketConnection={createWebSocketConnection} />
+      <App />
     </Router>
   </Provider>
   , document.getElementById('root')
 );
-};
-
-renderDOM();
